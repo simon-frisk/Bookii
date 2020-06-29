0f5a1ffbe1eeb3380cf1f5c2f83c12c2434f41af
Simon Frisk <simon@simonfrisk.com>
2020-06-29 18:43:57 +0200

    Update style FeedBookCard

--- src/components/bookcard/FeedBookCard/ExpandableText.js
+++ src/components/bookcard/FeedBookCard/ExpandableText.js
@@ -4,12 +4,22 @@ import { Text, TouchableOpacity } from 'react-native'
 export default ({ text }) => {
   const [isExpanded, setIsExpanded] = useState(false)
 
-  const extract = text.slice(0, 120).split(' ').slice(0, -1).join(' ')
+  if (text.length < 130)
+    return <Text style={{ marginVertical: 10 }}>{text}</Text>
+
+  const extract = text
+    .slice(0, 90)
+    .split(' ')
+    .slice(0, -1)
+    .join(' ')
+    .concat(' ...')
 
   return (
     <>
       <TouchableOpacity onPress={() => setIsExpanded(!isExpanded)}>
-        <Text style={{ marginTop: 10 }}>{isExpanded ? text : extract}</Text>
+        <Text style={{ marginVertical: 10 }}>
+          {isExpanded ? text : extract}
+        </Text>
         <Text style={{ color: 'blue' }}>
           {isExpanded ? 'Read less' : 'Read more'}
         </Text>
