a9c9cf2047d77e16bd015e71c1ebbfa7e254bd5e
Simon Frisk <simon@simonfrisk.com>
2020-06-30 13:05:54 +0200

    Delete account are you sure button

--- src/pages/Profile/DeleteProfile.js
+++ src/pages/Profile/DeleteProfile.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Alert } from 'react-native'
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import PressButton from '../../components/PressButton'
@@ -20,13 +21,31 @@ export default ({ _id }) => {
   const errorMessage = useApolloError(error)
   const { signout } = useContext(AuthContext)
 
+  const showAlert = () => {
+    Alert.alert(
+      'Delete account',
+      'This will remove your account and all data. This cannot be undone.',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete account',
+          style: 'destructive',
+          onPress: deleteAccount,
+        },
+      ]
+    )
+  }
+
   return (
     <>
       <PressButton
-        text='Delete profile'
+        text='Delete account'
         type='error'
         loading={loading}
-        onPress={deleteAccount}
+        onPress={showAlert}
       />
       {error && <Text style={{ color: 'red' }}>{errorMessage}</Text>}
     </>
